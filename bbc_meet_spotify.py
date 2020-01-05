8423c62d26e14ce8b71da477675b088bd0ea1c5f
Stef Piatek <stefan@piatek.co.uk>
2020-01-05 18:38:51 +0000

    Make code pep8 compliant

--- bbc_meet_spotify.py
+++ bbc_meet_spotify.py
@@ -1,15 +1,15 @@
 import re
+import time
+import unicodedata
+from pathlib import Path
+from typing import List
+
 import requests
 import spotipy
-import time
 import toml
-import unicodedata
-
 from bs4 import BeautifulSoup
 from loguru import logger
-from pathlib import Path
 from spotipy import util
-from typing import List
 
 
 class BBCSounds:
@@ -23,7 +23,6 @@ class BBCSounds:
         playlists = toml.load(Path("./bbc_playlists.toml"))
         return playlists[playlist_key]
 
-
     @staticmethod
     def get_songs(url: str) -> dict:
         """
@@ -32,10 +31,12 @@ class BBCSounds:
         :return: List of songs
         """
         page = requests.get(url)
-        soup = BeautifulSoup(page.text, 'html.parser')
+        soup = BeautifulSoup(page.text, "html.parser")
         song_grid = soup.find(class_="programmes-page article--individual")
         artists = [i.text.strip() for i in song_grid.find_all_next("a", class_="br-blocklink__link promotion__link")]
-        song_title = [i.text.strip() for i in song_grid.find_all_next("p", class_="promotion__synopsis centi text--subtle")]
+        song_title = [
+            i.text.strip() for i in song_grid.find_all_next("p", class_="promotion__synopsis centi text--subtle")
+        ]
         songs = [Song(artist, track_name) for track_name, artist in zip(song_title, artists)]
         # remove last item because this is an album
         logger.info(f"Album of the day is is: {songs.pop(-1)}")
@@ -59,11 +60,10 @@ class Song:
         :param string: input string to be cleaned
         :return: cleaned string
         """
-        new_string = ''.join(
-            [char for char in unicodedata.normalize('NFD', string)
-             if unicodedata.category(char) != 'Mn']
+        new_string = "".join(
+            [char for char in unicodedata.normalize("NFD", string) if unicodedata.category(char) != "Mn"]
         )
-        new_string = re.sub("[^A-Za-z0-9\']+", " ", new_string)
+        new_string = re.sub("[^A-Za-z0-9']+", " ", new_string)
         return new_string
 
 
@@ -92,10 +92,16 @@ class Spotify:
         :param config: configuration
         :return: spotify OAuth token
         """
-        token = util.prompt_for_user_token(config["username"], "playlist-modify-private playlist-modify-public", config["client_id"], config["client_secret"], "http://localhost:8888")
+        token = util.prompt_for_user_token(
+            config["username"],
+            "playlist-modify-private playlist-modify-public",
+            config["client_id"],
+            config["client_secret"],
+            "http://localhost:8888",
+        )
         return token
 
-    def create_playlist(self, playlist_name: str, add_date_prefix: bool=True, public_playlist: bool=True) -> str:
+    def create_playlist(self, playlist_name: str, add_date_prefix: bool = True, public_playlist: bool = True) -> str:
         """
         Creates playlist if it doesn't already exist, otherwise get the playlist id
         :param playlist_name: name for the playlist
@@ -110,14 +116,14 @@ class Spotify:
         playlist = {}
         for current_playlist in current_playlists["items"]:
             if playlist_name == current_playlist["name"]:
-                playlist["id"]  = current_playlist["id"]
+                playlist["id"] = current_playlist["id"]
                 logger.info(f"Playlist '{playlist_name}' already exists, reusing playlist")
 
         if not playlist:
             logger.info(f"Creating playlist '{playlist_name}' for user '{self.username}'")
             playlist = self.spotify.user_playlist_create(self.username, playlist_name, public=public_playlist)
 
-        return playlist['id']
+        return playlist["id"]
 
     def get_song_ids(self, songs: List[Song]) -> List[str]:
         """
@@ -138,7 +144,7 @@ class Spotify:
         :param add_date_prefix: If true, add date prefix to playlist
         :param public_playlist: If true, make playlist public
         """
-        playlist_id =  self.create_playlist(playlist_name, add_date_prefix, public_playlist)
+        playlist_id = self.create_playlist(playlist_name, add_date_prefix, public_playlist)
         song_ids = self.get_song_ids(songs)
         self.add_songs_to_playlist(playlist_id, song_ids)
         logger.info("All done, if you have any errors then you'll have to add these manually I'm afraid")
@@ -153,20 +159,19 @@ class Spotify:
         """
         song_id = None
         try:
-            results = self.spotify.search(
-                q=f"artist:'{song.artist}' track:'{song.song_title}'")
-            song_id = results['tracks']['items'][0]['id']
+            results = self.spotify.search(q=f"artist:'{song.artist}' track:'{song.song_title}'")
+            song_id = results["tracks"]["items"][0]["id"]
         except IndexError:
             # try fixing the strings with removing apostrophes
             new_song_title = song.song_title.replace("'", "")
             new_artist = song.artist.replace("'", "")
             try:
-                results = self.spotify.search(
-                    q=f"artist:'{new_artist}' track:'{new_song_title}'")
-                song_id = results['tracks']['items'][0]['id']
+                results = self.spotify.search(q=f"artist:'{new_artist}' track:'{new_song_title}'")
+                song_id = results["tracks"]["items"][0]["id"]
             except IndexError as e:
                 logger.error(f"Could not find a song: {song}", e)
         return song_id
+
 
 @logger.catch
 def main(playlist_key="6music", date_prefix=True, public_playlist=True):
@@ -176,5 +181,5 @@ def main(playlist_key="6music", date_prefix=True, public_playlist=True):
     spotify.main(playlist_info["verbose_name"], songs, date_prefix, public_playlist)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
