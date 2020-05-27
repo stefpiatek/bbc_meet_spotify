003d6be585fb9b9f27d29e69e52754420f90bc60
Stef Piatek <stefan@piatek.co.uk>
2020-05-27 21:32:00 +0100

    Add version callback to cli

--- src/bbc_meet_spotify/console.py
+++ src/bbc_meet_spotify/console.py
@@ -4,24 +4,35 @@ import typer
 from loguru import logger
 from spotipy import Spotify
 
-from . import BBCSounds, Spotify
+from . import BBCSounds, Spotify, __version__
 
 
 class PlaylistChoices(str, Enum):
     six_music = "six_music"
     radio1 = "radio1"
 
+
+def version_callback(value: bool):
+    if value:
+        typer.echo(f"BBC meet Spotify version: {__version__}")
+        raise typer.Exit()
+
+
 @logger.catch
-def console(playlist_key: PlaylistChoices,
-         date_prefix: bool = typer.Option(False,
-                                          help="Add a date prefix to be added to your spotify playlist?",
-                                          show_default=True),
-         public_playlist: bool = typer.Option(True, "--public-playlist/--private-playlist",
-                                              help="Spotify playlist settings",
-                                              show_default=True),
-         custom_playlist_name: str = typer.Option(None, "--custom-playlist-name", "-n",
-                                                  help="Set a custom name for playlist")
-         ):
+def console(
+        playlist_key: PlaylistChoices,
+        date_prefix: bool = typer.Option(False,
+                                         help="Add a date prefix to be added to your spotify playlist?",
+                                         show_default=True),
+        public_playlist: bool = typer.Option(True, "--public-playlist/--private-playlist",
+                                             help="Spotify playlist settings",
+                                             show_default=True),
+        custom_playlist_name: str = typer.Option(None, "--custom-playlist-name", "-n",
+                                                 help="Set a custom name for playlist"),
+        version: bool = typer.Option(
+            None, "--version", callback=version_callback, is_eager=True
+        ),
+):
     logger.info(f"Getting playlist for bbc playlist key {playlist_key.value}")
     bbc_sounds = BBCSounds(playlist_key.value, date_prefix, custom_playlist_name)
 
