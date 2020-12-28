dda427291afea8872b292d6d627c66d5ff4484cd
Stef Piatek <s.piatek@ucl.ac.uk>
2020-12-28 17:46:44 +0000

    Fix typing

--- src/bbc_meet_spotify/playlist_parsing.py
+++ src/bbc_meet_spotify/playlist_parsing.py
@@ -1,11 +1,11 @@
 from enum import Enum
 from pathlib import Path
-from typing import List
+from typing import Tuple
 
 import toml
 
 
-def parse_playlist_file(playlist_key:str, custom_playlist_name: str = None) -> List[str, str]:
+def parse_playlist_file(playlist_key: str, custom_playlist_name: str = None) -> Tuple[str, str]:
     """
     Parses playlist file, with optional custom playlist name override
     :param playlist_key:
@@ -15,6 +15,7 @@ def parse_playlist_file(playlist_key:str, custom_playlist_name: str = None) -> L
     playlist = toml.load(Path("./bbc_playlists.toml"))[playlist_key]
     playlist_suffix = custom_playlist_name or playlist["verbose_name"]
     return playlist["url"], playlist_suffix
+
 
 class PlaylistChoices(str, Enum):
     six_music = "six_music"
