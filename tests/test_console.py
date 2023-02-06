aeff976368b379fe3fbd1cb836b9c493be7d69fb
Sean Kenna <sean.kenna@skyscanner.net>
2023-02-06 12:08:45 +0000

    Exit early when no new music to add to Spotify

--- tests/test_console.py
+++ tests/test_console.py
@@ -42,3 +42,19 @@ def test_add_albums(mock_spotify: MagicMock, mock_bbc_sounds: MagicMock):
     mock_spotify_instance.add_albums.assert_called_with(playlist_name, music, ANY, ANY)
     mock_spotify_instance.add_songs.assert_not_called()
     mock_bbc_sounds_instance.write_playlist_history.assert_called_with(music)
+
+
+@patch("bbc_meet_spotify.console.BBCSounds")
+@patch("bbc_meet_spotify.console.Spotify")
+def test_exits_when_no_new_music(mock_spotify: MagicMock, mock_bbc_sounds: MagicMock):
+    mock_bbc_sounds_instance = mock_bbc_sounds.return_value
+    mock_spotify_instance = mock_spotify.return_value
+    music = []
+    mock_bbc_sounds_instance.get_music.return_value = music
+    playlist_name = "suffix"
+    mock_bbc_sounds_instance.playlist_suffix = playlist_name
+    console(PlaylistChoices("six_music"))
+    mock_bbc_sounds_instance.get_music.assert_called_once()
+    mock_spotify_instance.add_albums.assert_not_called()
+    mock_spotify_instance.add_songs.assert_not_called()
+    mock_bbc_sounds_instance.write_playlist_history.assert_not_called()
